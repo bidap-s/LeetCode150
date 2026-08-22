3aad5edff786b95b269cc3145277ecbaede096be
bidap-s <bidapsneha@gmail.com>
2026-08-22 22:18:55 +0530

    Time: 2 ms (43.58%), Space: 94.4 MB (51.7%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+    int minProfit=prices[0];
+    int maxProfit=0;
+    int profit=0;
+    
+    for(int i=1;i<prices.length;i++){
+        if(prices[i]<minProfit){
+            minProfit=prices[i];
+        }else{
+            profit=prices[i]-minProfit;
+        }
+        if(profit>maxProfit){
+            maxProfit=profit;
+        }
+    }  
+    return maxProfit;
+    }
+}
